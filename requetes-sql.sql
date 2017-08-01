414cab52e5fc626e68ea403c475a15556b6dcb94
formation <toto@toto.com>
2017-08-01 15:33:59 +0200

    liste des requetes

---
+++ requetes-sql.sql
@@ -0,0 +1,18 @@
+# cumul des ventes par années
+SET @cumul := 0, @rang := 0;
+
+SELECT
+@rang := @rang + 1 as rang,
+t.total,
+(@cumul := @cumul + t.total) AS cumul
+
+FROM (
+		SELECT
+		YEAR(date_vente) as annee,
+		SUM(montant) AS total,
+		
+		COUNT(*) AS nb_ventes
+		FROM ventes
+		GROUP BY annee
+		ORDER BY annee DESC
+) AS t
